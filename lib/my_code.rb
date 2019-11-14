3d70bd5f79bbb1fcea28240a2cb0122e4812bc7c
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 19:35:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map_to_negativize(source_array)
   result
 end
 
+def map_to_no_change(source_array)
+  result = []
+  i = 0
+  while i < source_array.length do
+    result << source_array[i]
+    i += 1
+  end
+  result
+end
