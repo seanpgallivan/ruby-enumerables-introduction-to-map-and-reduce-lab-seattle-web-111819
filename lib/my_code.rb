bed3276b5bf899e2c50d712aca089adbf2bcd0b0
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 19:36:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,23 @@ def map_to_no_change(source_array)
   end
   result
 end
+
+def map_to_double(source_array)
+  result = []
+  i = 0
+  while i < source_array.length do
+    result << source_array[i] * 2
+    i += 1
+  end
+  result
+end
+
+def map_to_square(source_array)
+  result = []
+  i = 0
+  while i < source_array.length do
+    result << source_array[i] ** 2
+    i += 1
+  end
+  result
+end
